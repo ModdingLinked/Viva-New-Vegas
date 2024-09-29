aa1e22c4ab0a1e949816b05e62fe3b683ee065d6
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2024-09-29 18:18:13 +0200

    Remove PWA elements

--- javascript.js
+++ javascript.js
@@ -311,30 +311,6 @@ document.addEventListener('click', function (event) {
     }
 });
 
-const registerServiceWorker = async () => {
-    if ("serviceWorker" in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register("./service-worker.js", {
-                scope: "/",
-            });
-            if (registration.installing) {
-                console.log("Service worker installing");
-            } else if (registration.waiting) {
-                console.log("Service worker installed");
-            } else if (registration.active) {
-                console.log("Service worker active");
-            }
-        } catch (error) {
-            console.error(`Registration failed with ${error}`);
-        }
-    }
-};
-
-// …
-
-registerServiceWorker();
-
-
 // Keylogger by yours truly
 document.addEventListener('keydown', function (event) {
     if (event.key === konamiCode[konamiCodeIndex]) {
